b95e758480bcd4f315119c005d7d7e7514aaeec0
sajid-31 <sajidm3107@gmail.com>
2025-03-22 15:47:04 +0000

    feat: created json based prompt method for intelligence

--- chopper/formatter/response_formatter.py
+++ chopper/formatter/response_formatter.py
@@ -1,4 +1,20 @@
+import json
+
 class ResponseFormatter:
     def format(self, llm_response):
         # Simple formatting, can be enhanced
-        return llm_response['choices'][0]['message']['content'].strip()
+        response = llm_response['choices'][0]['message']['content'].strip()
+        # print(response)
+        response = json.loads(response)
+        if response['TYPE']=='AMBIGUOUS':
+            return 'Your question does not align with my area of expertise'
+        elif response['TYPE']=='CODE':
+            result = ""
+            for command_dict in response['CODE']:
+                for desc,command in command_dict.items():
+                    result = result +'➡️  ' + desc + '\n' + '\033[92m'+command + '\033[0m' + '\n'
+            return result
+        
+        elif response['TYPE']=='CONCEPT':
+            return response['INFO']
+           
--- chopper/main.py
+++ chopper/main.py
@@ -4,6 +4,7 @@ from formatter.response_formatter import ResponseFormatter
 from executor.command_chooser import CommandChooser
 from executor.command_executor import CommandExecutor
 from system_info.system_info_fetcher import fetch_system_info
+from pathlib import Path
 def main():
     print("🔧 CHOPPER.AI Terminal Started!")
 
@@ -13,13 +14,16 @@ def main():
 
     while True:
         user_input = ui.get_user_input()
+        base_path = Path(__file__).parent
+        file_path = base_path / 'prompts' / 'chopper_help.txt'
+        with file_path.open('r') as file:
+            prompt = file.read()
+        
+        user_input = prompt + user_input
         llm_response = llm.query(user_input)
         formatted_response = formatter.format(llm_response)
         ui.display_output(formatted_response)
 
-        command = CommandChooser.choose_command(formatted_response)
-        if command:
-            CommandExecutor.execute(command)
 
 
 if __name__ == "__main__":
